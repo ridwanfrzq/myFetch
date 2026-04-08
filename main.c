3b9d6a6d345748bed46ad5278513549048d51772
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-04-08 20:56:55 +0700

    fopen null check

--- main.c
+++ main.c
@@ -4,48 +4,55 @@
 
 void get_os() {
   FILE *file = fopen("/etc/os-release", "r");
-  char line[256];
-
-  while (fgets(line, sizeof(line), file)) {
-    if (strncmp(line, "PRETTY_NAME=", 12) == 0) {
-      char *value = strchr(line, '=');
-      if (!value) break;
-
-      value++;
-
-      value[strcspn(value, "\n")] = '\0';
-
-      if (*value == '"') value++;
-
-      size_t len = strlen(value);
-      if (len > 0 && value[len - 1] == '"')
-        value[len - 1] = '\0';
-      printf("OS: %s\n", value);
-      break;
+    
+    if (!file) {
+        printf("OS: Unknown\n");
+        return;
     }
-  }
-  fclose(file);
+    char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        if (strncmp(line, "PRETTY_NAME=", 12) == 0) {
+        char *value = strchr(line, '=');
+        if (!value) break;
+        value++;
+        value[strcspn(value, "\n")] = '\0';
+        if (*value == '"') value++;
+        size_t len = strlen(value);
+        if (len > 0 && value[len - 1] == '"')
+            value[len - 1] = '\0';
+        printf("OS: %s\n", value);
+        break;
+        }
+    }
+    fclose(file);
 }
 
 void get_kernel() {
-  struct utsname u;
-  uname(&u);
-  printf("Kernel: %s %s\n", u.sysname, u.release);
+    struct utsname u;
+    uname(&u);
+    printf("Kernel: %s %s\n", u.sysname, u.release);
 }
 
 void get_hostname() {
-  FILE *file = fopen("/etc/hostname", "r");
-  char line[50];
+    FILE *file = fopen("/etc/hostname", "r");
+
+    if (!file) {
+        printf("Hostname: Unknown\n");
+        return;
+    }
+
+    char line[50];
   
-  fgets(line, sizeof(line), file);
-  line[strcspn(line, "\n")] = '\0';
-  printf("Hostname: %s\n", line);
-  fclose(file);
+    fgets(line, sizeof(line), file);
+    line[strcspn(line, "\n")] = '\0';
+    printf("Hostname: %s\n", line);
+    fclose(file);
 }
 
 int main() {
-  get_os();
-  get_kernel();
-  get_hostname();
-  return 0;
+    get_os();
+    get_kernel();
+    get_hostname();
+    return 0;
 }
