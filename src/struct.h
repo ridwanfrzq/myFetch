4192520971e6cb59a0baa637ca910d7877e42bfb
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-04-27 23:57:19 +0700

    add struct header

---
+++ src/struct.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_h
+#define STRUCT_H
+
+typedef struct {
+    char user[50];
+    char os[50];
+    char kernel[50];
+    char hostname[50];
+    char shell[50];
+    char cpu[50];
+    char memory[50];
+    char uptime[50];
+} Info;
+
+#endif
